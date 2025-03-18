050b609d44507450b43c8c2ca4f0c6933a83811e
Guilherme de Jesus Silva <guilhermedejesussilva@AirdeGuilherme.home>
2025-03-18 16:47:29 -0300

    Correção Tela

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -11,7 +11,7 @@ export default function TaskPage() {
   return (
     <div>
       <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-        <div className="w-[500px] space-y-4">
+        <div className="w-[500px] mx-auto space-y-4">
           <div className="flex justify-center relative md-6">
             <button
               onClick={() => navigate(-1)}
